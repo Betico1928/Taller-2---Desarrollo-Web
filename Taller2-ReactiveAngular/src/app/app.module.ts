ef8377d5c4d843db88eea8b0fb77d9428a193223
Betico1928 <betoveluis@gmail.com>
2023-10-01 17:29:33 -0500

    Creado el componente de buscar usuario

--- Taller2-ReactiveAngular/src/app/app.module.ts
+++ Taller2-ReactiveAngular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './component/user-search/user-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSearchComponent
   ],
   imports: [
     BrowserModule
