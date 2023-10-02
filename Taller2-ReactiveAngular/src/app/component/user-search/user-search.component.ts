573bc9a04e6e4e07a66788f55e5098592726472e
Betico1928 <betoveluis@gmail.com>
2023-10-01 19:51:14 -0500

    Modal de error implementado. Diseño de la página mejorado.

--- Taller2-ReactiveAngular/src/app/component/user-search/user-search.component.ts
+++ Taller2-ReactiveAngular/src/app/component/user-search/user-search.component.ts
@@ -13,22 +13,29 @@ export class UserSearchComponent {
   username: string = '';
   user: Users | null = null;
   error: string = '';
-
+  showErrorModal: boolean = false;  // Añadido para manejar la visualización del modal
 
   constructor(private apiService: ApiService) { }
 
   searchUserByUsername() {
-    this.apiService.searchUserByUsername(this.username).subscribe(
-      data => {
+    this.apiService.searchUserByUsername(this.username).subscribe(data => {
         if (data.users && data.users.length > 0) {
           this.user = data.users[0];
+          this.showErrorModal = false;  // Asegurarse de que el modal esté oculto
         } else {
           this.error = 'Usuario no encontrado';
+          this.showErrorModal = true;
+          setTimeout(() => {
+            this.showErrorModal = false;  // Ocultar el modal después de 5 segundos
+          }, 5000);
         }
       },
       err => {
         this.error = 'Hubo un error en la búsqueda';
-      }
-    );
+        this.showErrorModal = true;
+        setTimeout(() => {
+          this.showErrorModal = false;
+        }, 5000);
+      });
   }
 }
